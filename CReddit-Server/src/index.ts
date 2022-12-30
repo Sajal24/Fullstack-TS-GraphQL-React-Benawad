7d293f617b26de6e925a08be17c4c32fa0fa060a
Sajal Sharma <sajalsharma2408@gmail.com>
2022-12-31 03:07:07 +0530

    commit

--- CReddit-Server/src/index.ts
+++ CReddit-Server/src/index.ts
@@ -1,14 +1,17 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import mikroConfig from "./mikro-orm.config";
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    entities: [Post],
-    dbName: "creddit",
-    type: "postgresql",
-    debug: !__prod__,
-  });
+  const orm = await MikroORM.init(mikroConfig);
+
+  const post = orm.em.create(Post, { title: "my first post" });
+  await orm.em.persistAndFlush(post);
 };
 
-main();
+main().catch((err) => {
+  console.error(err);
+});
+
+//llkl
