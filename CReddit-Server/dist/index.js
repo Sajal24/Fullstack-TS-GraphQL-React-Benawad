9d4db8164b26793aad6a95fbae27003718ec884d
Sajal Sharma <sajalsharma2408@gmail.com>
2022-12-30 19:21:46 +0530

    learning new stugg

--- CReddit-Server/dist/index.js
+++ CReddit-Server/dist/index.js
@@ -1,4 +1,15 @@
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@mikro-orm/core");
+const constants_1 = require("./constants");
+const Post_1 = require("./entities/Post");
+const main = async () => {
+    const orm = await core_1.MikroORM.init({
+        entities: [Post_1.Post],
+        dbName: "creddit",
+        type: "postgresql",
+        debug: !constants_1.__prod__,
+    });
+};
+main();
 //# sourceMappingURL=index.js.map
--- CReddit-Server/src/index.ts
+++ CReddit-Server/src/index.ts
@@ -1,3 +1,14 @@
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [Post],
+    dbName: "creddit",
+    type: "postgresql",
+    debug: !__prod__,
+  });
+};
+
+main();
